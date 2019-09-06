3eb987d91eb5f33e2f03d8d6d693c0b039632b85
Dmitry <n.dmitriy.li@mail.ru>
2019-09-06 18:04:54 +0300

    Осуществил отправку формы через AJAX

---
+++ src/js/scripts.js
@@ -0,0 +1,15 @@
+/* Обработка и отправка формы через AJAX */
+$('#offer-form').on('submit', function(event) {
+	event.preventDefault();
+	$.ajax({
+		url: 'mail.php',
+		type: 'POST',
+		data: $(this).serialize(),
+		success: function(data) {
+			$('.success').html(data + ', Ваша форма отправлена.')
+		},
+		error: function(jqXHR, textStatus) {
+			console.log(jqXHR + ': ' + textStatus);
+		}
+	})
+});
